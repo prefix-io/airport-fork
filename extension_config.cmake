66b4c13cf82620e82ec0ec3f0e7fb7694c1479c5
Rusty Conover <rusty@conover.me>
2025-07-14 19:50:59 -0400

    fix: add dont link

--- extension_config.cmake
+++ extension_config.cmake
@@ -9,6 +9,7 @@ duckdb_extension_load(airport
 duckdb_extension_load(httpfs
     APPLY_PATCHES
     LOAD_TESTS
+    DONT_LINK
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG 217ec8e04f6ed419c866a6d2496aa15aace4382f
     INCLUDE_DIR extension/httpfs/include
