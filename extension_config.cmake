457e2445a2ee37a6b97b96ab15521b64668caa47
Rusty Conover <rusty@conover.me>
2025-07-14 20:22:58 -0400

    fix: attempting ci fixes

--- extension_config.cmake
+++ extension_config.cmake
@@ -7,12 +7,12 @@ duckdb_extension_load(airport
 )
 
 duckdb_extension_load(httpfs
-    LOAD_TESTS
+#    LOAD_TESTS
     DONT_LINK
     GIT_URL https://github.com/duckdb/duckdb-httpfs
     GIT_TAG af7bcaf40c775016838fef4823666bd18b89b36b
-    INCLUDE_DIR extension/httpfs/include
-    )
+#    INCLUDE_DIR extension/httpfs/include
+)
 
 # Any extra extensions that should be built
 # e.g.: duckdb_extension_load(json)
